3f86d93ea9486c45868a0f015c772bcefb0be179
Joshua Tyler <josh.tyler@btinternet.com>
2019-10-21 21:04:36 +0100

    Add ability to rotate components

--- kicad_pos_to_cpl.py
+++ kicad_pos_to_cpl.py
@@ -7,8 +7,11 @@
 import sys
 import csv
 from collections import OrderedDict
+import kicad_netlist_reader
 
-with open(sys.argv[1], 'r') as in_file, open(sys.argv[2], 'w', newline='') as out_file:
+net = kicad_netlist_reader.netlist(sys.argv[1])
+
+with open(sys.argv[2], 'r') as in_file, open(sys.argv[3], 'w', newline='') as out_file:
 
     reader = csv.DictReader(in_file)
     ordered_fieldnames = OrderedDict([('Designator',None),('Mid X',None),('Mid Y',None),('Layer',None),('Rotation',None)])
@@ -16,10 +19,24 @@ with open(sys.argv[1], 'r') as in_file, open(sys.argv[2], 'w', newline='') as ou
     writer.writeheader()
 
     for row in reader:
+        # For each component, get the rotation field from the schematic, and add it to the rotation
+        done = False
+        rot = int(float(row['Rot']))
+        for group in net.groupComponents():
+            for component in group:
+                if component.getRef() == row['Ref']:
+                    if component.getField('JLCPCB Rotation'):
+                        rot = rot + int(float(component.getField('JLCPCB Rotation')))
+                    done = True
+                if done:
+                    break
+            if done:
+                break
+        assert done
         writer.writerow({
             'Designator': row['Ref'], 
             'Mid X': row['PosX'] + 'mm', 
             'Mid Y': row['PosY'] + 'mm', 
             'Layer': row['Side'].capitalize(), 
-            'Rotation': int(float(row['Rot']))
+            'Rotation': rot
         })
